3ce98517dbf82874cd452246543c85319fd18ba1
Amadeus <adek-g@go2.pl>
2019-04-10 19:14:57 +0200

    working with canvas

--- src/components/header.js
+++ src/components/header.js
@@ -1,33 +1,16 @@
-import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
+import Helmet from "react-helmet"
+import { withPrefix } from "gatsby"
 
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
+  <header>
+    <Helmet>
+    <script src="http://requirejs.org/docs/release/2.1.15/minified/require.js" type="text/javascript" />
+    <script src="https://rawgit.com/ironwallaby/delaunay/master/delaunay.jsgit " type="text/javascript" />
+    <script src={withPrefix('canvas.js')} type="text/javascript" />
+    </Helmet>
+    <canvas id="stars" width="300" height="300"></canvas>
   </header>
 )
 
