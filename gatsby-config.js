59feff2fb0f4efc9670e8908414c5e72f4fe003c
Amadeus <adek-g@go2.pl>
2019-05-14 22:34:50 +0200

    added google analitics

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,6 +14,12 @@ module.exports = {
         path: `${__dirname}/src/images`
       }
     },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-140137342-1",
+      },
+    },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
