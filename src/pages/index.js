f29e1309cb0c50373bbd9e8940d980c6abc78ae6
Amadeus <adek-g@go2.pl>
2019-08-11 20:39:02 +0200

    added description

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,15 @@
 import React from "react"
-
+import Helmet from "react-helmet";
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Amadesz Grzesiak - Front-end Developer | Strony i sklepy internetowe" description="Cześć, nazywam się Amadeusz i jestem kreatywnym front-end developerem z Jeleniej Góry. Stwórzmy coś razem!" keywords={[`front-end`, `developer`, `wrocław`, 'strony internetowe', 'sklepy internetowe']} />
-
+    <Helmet>
+        <title>Amadesz Grzesiak - Front-end Developer | Strony i sklepy internetowe</title>
+        <meta name="description" content="Cześć, nazywam się Amadeusz i jestem kreatywnym front-end developerem z Jeleniej Góry. Stwórzmy coś razem!" />
+    </Helmet>
   </Layout>
 )
 
